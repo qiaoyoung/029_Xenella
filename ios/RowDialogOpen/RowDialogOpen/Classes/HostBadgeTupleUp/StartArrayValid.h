d69f22a04de0f0dc22071497525a76fe94648673
Joe <joeyoung@guojiang.tv>
2025-11-13 18:17:00 +0800

    1.0.1.

---
+++ ios/RowDialogOpen/RowDialogOpen/Classes/HostBadgeTupleUp/StartArrayValid.h
@@ -0,0 +1,82 @@
+// __DEBUG__
+// __CLOSE_PRINT__
+//
+//  StartArrayValid.h
+// TaskIdentifyRave
+//
+//  Created by chris.
+//  Copyright (c) 2015年 NetEase. All rights reserved.
+//
+//: #import <Foundation/Foundation.h>
+#import <Foundation/Foundation.h>
+//: #import <Photos/Photos.h>
+#import <Photos/Photos.h>
+
+// __M_A_C_R_O__
+
+//: @class FFFMediaItem;
+@class AdminPaper;
+
+
+//: @protocol NIMInputActionDelegate <NSObject>
+@protocol OftentimesTurn <NSObject>
+
+//: @optional
+@optional
+//: - (BOOL)onTapMediaItem:(FFFMediaItem *)item;
+- (BOOL)overvaliantses:(AdminPaper *)item;
+
+//: - (void)onTextChanged:(id)sender;
+- (void)specialShadow:(id)sender;
+
+//: - (void)onSendText:(NSString *)text
+- (void)notice:(NSString *)text
+           //: atUsers:(NSArray *)atUsers;
+           entry:(NSArray *)atUsers;
+
+//: - (void)onSelectChartlet:(NSString *)chartletId
+- (void)pen:(NSString *)chartletId
+                 //: catalog:(NSString *)catalogId;
+                 move:(NSString *)catalogId;
+
+//: - (void)onSendText:(NSString *)text atUsers:(NSArray *)atUsers replymessage:(NIMMessage *)replymessage;
+- (void)springReplymessage:(NSString *)text yield:(NSArray *)atUsers session:(NIMMessage *)replymessage;
+
+//: - (void)onSelectEmoticon:(id)emoticon;
+- (void)mark:(id)emoticon;
+
+//: - (void)onCancelRecording;
+- (void)referLoad;
+
+//: - (void)onStopRecording;
+- (void)signalRecording;
+
+//: - (void)onStartRecording;
+- (void)laboratoryEven;
+
+//: - (void)onTapMoreBtn:(id)sender;
+- (void)query:(id)sender;
+
+//: - (void)onTapEmoticonBtn:(id)sender;
+- (void)lowballBtn:(id)sender;
+
+//: - (void)onTapAudioBtn:(id)sender;
+- (void)alongs:(id)sender;
+
+//: - (void)onTapAlbunBtn:(id)sender;
+- (void)intervalled:(id)sender;
+
+//: - (void)onTapCameraBtn:(id)sender;
+- (void)theStart:(id)sender;
+
+//: - (void)didReplyCancelled;
+- (void)infoCancelled;
+
+//: - (void)onTapPraiseItem:(NSInteger)index withMessage:(NIMMessage *)message;
+- (void)glass:(NSInteger)index have:(NIMMessage *)message;
+
+//: - (void)onTapAlbunArray:(NSArray<PHAsset *> *)assets;
+- (void)instanced:(NSArray<PHAsset *> *)assets;
+
+//: @end
+@end
