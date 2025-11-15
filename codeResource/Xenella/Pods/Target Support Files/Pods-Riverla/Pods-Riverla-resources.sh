9d5766e0c6cce6102a9cdae69ee562fd46ed3282
Joe <joeyoung@guojiang.tv>
2025-11-15 21:25:02 +0800

    修改资源

--- codeResource/Xenella/Pods/Target Support Files/Pods-Riverla/Pods-Riverla-resources.sh
+++ codeResource/Xenella/Pods/Target Support Files/Pods-Riverla/Pods-Riverla-resources.sh
@@ -97,10 +97,10 @@ EOM
   esac
 }
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_resource "${PODS_ROOT}/../HolisticSaver/HolisticSaver/Resources/HolisticSaver.bundle"
+  install_resource "${PODS_ROOT}/../AmongConductor/AmongConductor/Resources/AmongConductor.bundle"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_resource "${PODS_ROOT}/../HolisticSaver/HolisticSaver/Resources/HolisticSaver.bundle"
+  install_resource "${PODS_ROOT}/../AmongConductor/AmongConductor/Resources/AmongConductor.bundle"
 fi
 
 mkdir -p "${TARGET_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}"
