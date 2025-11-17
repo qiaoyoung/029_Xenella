4c789c799cb11eeca906e68991a791ed3d63e401
Joe <joeyoung@guojiang.tv>
2025-11-17 15:04:36 +0800

    1.0.1+4

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -1,30 +1,15 @@
 #import "AppDelegate.h"
 #import <Flutter/Flutter.h>
 #import "GeneratedPluginRegistrant.h"
-//#import "FolderThroughout.h"
-#import "BaselineAbleNear.h"
-#import "NodeBuilderSet.h"
-#import "LaneHandlerBasin.h"
-#import "InspectorBeneathSendStretch.h"
-#import "HandlerShadowNavigationBar.h"
-#import "FlagshipWillowDuring.h"
 
 @implementation AppDelegate
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
-//    [[FolderThroughout previous] executed:application];
 }
 
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     [GeneratedPluginRegistrant registerWithRegistry:self];
-    [BaselineAbleNear scrollBrilliantSchedulerDaleTexture];
-    [NodeBuilderSet indexOrganizer];
-    [LaneHandlerBasin intoTooltipNaturalParser];
-    [InspectorBeneathSendStretch continueAssemblerWorldMapper];
-//    [[FolderThroughout previous] initCycle:self.window];
-    [HandlerShadowNavigationBar sendFaithfulHoneyMaterial];
-    [FlagshipWillowDuring downViewModelSaver];
     return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
@@ -34,7 +19,6 @@
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application {
-//    [[FolderThroughout previous] takeWaterDown:application];
 }
  
 - (BOOL)application:(UIApplication *)app openURL:(NSURL *)url options:(NSDictionary<NSString*, id> *)options {
@@ -42,7 +26,6 @@
 }
  
 - (void)application:(UIApplication *)app didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
-//    [[FolderThroughout previous] registerProperty:app index:deviceToken];
 }
 
 @end
