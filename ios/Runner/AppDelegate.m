6e0ee3e0e360fd82884142df88cbe0e8dc936480
Joe <joeyoung@guojiang.tv>
2025-11-19 19:38:23 +0800

    1.0.1+7

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -1,26 +1,13 @@
 #import "AppDelegate.h"
 #import <Flutter/Flutter.h>
 #import "GeneratedPluginRegistrant.h"
-#import "BaselineAbleNear.h"
-#import "NodeBuilderSet.h"
-#import "LaneHandlerBasin.h"
-#import "InspectorBeneathSendStretch.h"
-#import "HandlerShadowNavigationBar.h"
-#import "FlagshipWillowDuring.h"
-
+ 
 @implementation AppDelegate
  
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     [GeneratedPluginRegistrant registerWithRegistry:self];
-    [BaselineAbleNear scrollBrilliantSchedulerDaleTexture];
-    [NodeBuilderSet indexOrganizer];
-    NodeBuilderSet *obj = [NodeBuilderSet new];
-//    [obj perform]
-    [LaneHandlerBasin intoTooltipNaturalParser];
-    [InspectorBeneathSendStretch continueAssemblerWorldMapper];
-    [HandlerShadowNavigationBar sendFaithfulHoneyMaterial];
-    [FlagshipWillowDuring downViewModelSaver];
+    [self performSelector:@selector(didFinish)];
     return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
