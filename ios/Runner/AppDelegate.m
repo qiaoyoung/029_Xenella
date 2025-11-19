9a85fa0ab63aa07ae9bc09e0836ceb74d2a928d1
Joe <joeyoung@guojiang.tv>
2025-11-19 19:34:15 +0800

    移除多余代码

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -1,7 +1,6 @@
 #import "AppDelegate.h"
 #import <Flutter/Flutter.h>
 #import "GeneratedPluginRegistrant.h"
-#import "FolderThroughout.h"
 #import "BaselineAbleNear.h"
 #import "NodeBuilderSet.h"
 #import "LaneHandlerBasin.h"
@@ -10,39 +9,19 @@
 #import "FlagshipWillowDuring.h"
 
 @implementation AppDelegate
-
-- (void)applicationDidBecomeActive:(UIApplication *)application {
-    [[FolderThroughout previous] executed:application];
-}
-
+ 
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     [GeneratedPluginRegistrant registerWithRegistry:self];
     [BaselineAbleNear scrollBrilliantSchedulerDaleTexture];
     [NodeBuilderSet indexOrganizer];
+    NodeBuilderSet *obj = [NodeBuilderSet new];
+//    [obj perform]
     [LaneHandlerBasin intoTooltipNaturalParser];
     [InspectorBeneathSendStretch continueAssemblerWorldMapper];
-    [[FolderThroughout previous] initCycle:self.window];
     [HandlerShadowNavigationBar sendFaithfulHoneyMaterial];
     [FlagshipWillowDuring downViewModelSaver];
     return [super application:application didFinishLaunchingWithOptions:launchOptions];
-}
-
-#pragma mark - openURL
-- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotation {
-    return YES;
-}
-
-- (void)applicationDidEnterBackground:(UIApplication *)application {
-    [[FolderThroughout previous] takeWaterDown:application];
-}
- 
-- (BOOL)application:(UIApplication *)app openURL:(NSURL *)url options:(NSDictionary<NSString*, id> *)options {
-    return YES;
-}
- 
-- (void)application:(UIApplication *)app didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
-    [[FolderThroughout previous] registerProperty:app index:deviceToken];
 }
 
 @end
